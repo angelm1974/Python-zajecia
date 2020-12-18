9e41926df8b87ea098d07c7a1c56825f59c7f1ea
angelm1974 <18185521+angelm1974@users.noreply.github.com>
2020-12-18 18:20:13 +0100

    zadania_koniec

--- zadania na start/zadania_typ1.py
+++ zadania na start/zadania_typ1.py
@@ -1,4 +1,5 @@
 import time
+import locale
 # Korzystając z bibiloteki time oraz metody sleep():
 
 # Program 1
@@ -8,7 +9,7 @@ def count_down(number):
     for i in range(number):
         print(i+1)
         time.sleep(1)
-count_down(5)
+#count_down(5)
 
 # for a in range(1,6):
 #     time.sleep(1)
@@ -32,7 +33,7 @@ def dodawanie_do_zmiennej(number):
         time.sleep(1)
         
 #wywołanie metody z parametrem
-dodawanie_do_zmiennej(5)
+#dodawanie_do_zmiennej(5)
 
 # Program 3
 # Napisz podobny program który pobiera liczbę sekund od użytkownika i jeśli użytkownik podał
@@ -46,17 +47,42 @@ def pobierz_czekaj(number):
         print('Koniec czekania!')
     else:
         print('Nie będę czekał tak długo!')
-
+        
+def pobierz_czekaj2():
+    number=int(input("Podaj ile sekund mam poczekać:"))
+    if number<10:
+        print(f'Będę czekał {number} sekund.')
+        time.sleep(number)
+        print('Koniec czekania!')
+    else:
+        print('Nie będę czekał tak długo!')
 #wywołanie metody pobierz_czekaj
-pobierz_czekaj(int(input("Podaj ile sekund mam poczekać:")))
+#pobierz_czekaj2()
+
 # Program 4
 # Napisz program który przy pomocy bibiloteki time i funkcji gmtime() pobiera aktualną datę
+def pobierz_date():
+    data= time.gmtime()
+    print(data.tm_year,'-',data.tm_mon,'-',data.tm_mday)
+pobierz_date()
 
 # Program 5
 # Napisz program który przy pomocy bibiloteki time i funkcji strftime() pobiera aktualną datę
+def pobierz_pelna_date():
+    locale.setlocale(locale.LC_ALL,'')
+    print(time.strftime("%A %d %B %Y %H:%M:%S"))
 
 # Program 6
 # Napisz program używając metody time(), który zapamiętuje czas, a następnie prosi użytkownika
 # o wpisanie wyniku mnożenia liczb 5x6. Potem pobiera nowy czas, liczy różnicę czasów
 # i wyświetla je na ekranie.
+
+def czas_reakcji():
+    czas =time.time()
+    wynik=int(input("Podaj wynik działana 5x6:"))
+    czas2=time.time()
+    if wynik==30:
+        print(f'Potrzebowałeś {czas2-czas}sekund na rozwiązanie zadania')
+    else:
+        print(f'Popełniłeś błąd!!!')
 
