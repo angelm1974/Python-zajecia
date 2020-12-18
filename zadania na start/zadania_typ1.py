d5cdecea294110bba6b08213d4a09fe01b9cd123
angelm1974 <18185521+angelm1974@users.noreply.github.com>
2020-12-18 16:25:28 +0100

    Zadanie1

---
+++ zadania na start/zadania_typ1.py
@@ -0,0 +1,27 @@
+
+# Korzystając z bibiloteki time oraz metody sleep():
+
+# Program 1
+# Napisz program, który odlicza od 1 do 5, wypisując liczby co sekundę. 
+
+# Program 2
+# Napisz podobny program który odlicza od 1 do 5, wypisując liczby co sekundę. 
+# Dodatkowo liczba zapisywana jest do zmiennej T inkrementując ją za każdym razem o swoją wartość
+
+# Program 3
+# Napisz podobny program który pobiera liczbę sekund od użytkownika i jeśli użytkownik podał
+# liczbę mniejszą od 10 to program zatrzyma się na tak długo jaką użytkownik podał liczbę.
+# Jeżeli liczba jest większa od 10 to użytklownik otrzyma komunikat że program nie będzie 
+# czekał tak długo
+
+# Program 4
+# Napisz program który przy pomocy bibiloteki time i funkcji gmtime() pobiera aktualną datę
+
+# Program 5
+# Napisz program który przy pomocy bibiloteki time i funkcji strftime() pobiera aktualną datę
+
+# Program 6
+# Napisz program używając metody time(), który zapamiętuje czas, a następnie prosi użytkownika
+# o wpisanie wyniku mnożenia liczb 5x6. Potem pobiera nowy czas, liczy różnicę czasów
+# i wyświetla je na ekranie.
+
